76e2685dc723d50e62b7f43864c66cb6223cba9f
bang-a-coder <nikitas4717@gmail.com>
2021-12-07 14:58:32 +0100

    random walker took a walk and brought correct values

--- pagerank.py
+++ pagerank.py
@@ -5,17 +5,18 @@ import math
 
 network = {
 	"A": ["B"],
-	"B": ["A", "C"],
-	"C": ["A"]
+	"B": ["A"],
+	"C": ["A"],
+	"D": ["C"]
 }
 
 
-def pageRank(page, numerator):
+def pageRank_init(page, numerator, alpha, G):
 	res = 0
 	for inc_page in getLinks(page):
 		res += numerator[inc_page]/len(network[inc_page])
 
-	return res
+	return alpha/len(G) + (1-alpha) * res
 
 
 def getLinks(page):
@@ -27,10 +28,10 @@ def getLinks(page):
 	return links
 
 
-def powerIteration(G):
+def powerIteration_init(G):
 
 	iterations = defaultdict(dict)
-
+ 
 	for page in G:
 		iterations[0].update({page: 1/len(G.items())})
 
@@ -48,7 +49,7 @@ def powerIteration(G):
 	def iterator(k):
 		k += 1
 		for page in G.keys():
-				iterations[k].update({page: pageRank(page, iterations[k-1])})
+				iterations[k].update({page: pageRank_init(page, iterations[k-1], 0.15, G)})
 
 		if controller(0.01,k) == True: return iterator(k)
 		return
@@ -60,4 +61,20 @@ def powerIteration(G):
 
 
 # print(getLinks("A"))
-print(powerIteration(network))
+
+def fullPageRank(i, alpha, G, ranks):
+
+	sigma = 0
+	for inc_link in getLinks(i):
+		sigma += ranks[inc_link] / len(G[inc_link]) 
+	
+	return alpha/len(G) + (1-alpha) * sigma	
+
+
+
+init_ranks = powerIteration_init(network)
+
+print(fullPageRank("A", 0.15, network, init_ranks))
+print(fullPageRank("B", 0.15, network, init_ranks))
+print(fullPageRank("C", 0.15, network, init_ranks))
+print(fullPageRank("D", 0.15, network, init_ranks))
